9125bafa3a8b2c8cd8823a7c4368db6a54fdfa57
Mike Sokolov <msokolov@safaribooksonline.com>
2015-03-04 08:49:13 -0500

    draft of collapsing common suffixes

--- app/src/main/java/net/falutin/wordbog/DynamicLetterTrie.java
+++ app/src/main/java/net/falutin/wordbog/DynamicLetterTrie.java
@@ -3,8 +3,8 @@ package net.falutin.wordbog;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.Reader;
+import java.util.Map;
 import java.util.HashMap;
-import java.util.SortedMap;
 import java.util.TreeMap;
 
 /**
@@ -42,17 +42,51 @@ public class DynamicLetterTrie {
             isTerminal = false;
         }
 
-        static void add(String s, Node node) {
+        void add(String s, Node node) {
             for (int i = 0; i < s.length(); i++) {
                 char c = s.charAt(i);
-                Node child = node.children.get(c);
+                Node child = children.get(c);
                 if (child == null) {
                     child = new Node();
-                    node.children.put(c, child);
+                    children.put(c, child);
                 }
                 node = child;
             }
-            node.isTerminal = true;
+            isTerminal = true;
         }
+
+        /**
+         * @param suffixMap map of suffix to Node encoding the suffix, updated by this method
+         * and used to update the tree by collapsing common suffixes.
+         * @param suff partial suffix built up while descending unique branches of the tree
+         * @return the number of terminal nodes (ie words) on this branch
+         */
+        int collapse (StringBuilder suff, Map<CharSequence, Node> suffixMap) {
+            // TODO -- figure out how to accumulate suffixes efficiently
+            // perhaps passing in a StringBuilder
+            int terminalCount = isTerminal ? 1 : 0;
+            if (children.size() == 1) {
+                Map.Entry<Character,Node> child = children.firstEntry();
+                suff.append (child.getKey());
+                terminalCount += child.getValue().collapse (suff, suffixMap);
+            } else if (children.size() > 1) {
+                for (Map.Entry<Character,Node> child = children.entrySet()) {
+                    terminalCount += child.getValue().collapse (child.getKey(), suff);
+                    suff.setLength(0);
+                }
+            }
+            if (terminalCount == 1) {
+                addOrCollapse (suff.toString(), suffixMap);
+            }
+            return terminalCount;
+        }
+
     }
+
+    /** Converts the tree into a DAG by merging identical suffixes */
+    public void collapseSuffixes () {
+        HashMap<CharSequence,Node> suffixMap = new HashMap<>();
+        root.collapse (suffixMap);
+    }
+
 }
