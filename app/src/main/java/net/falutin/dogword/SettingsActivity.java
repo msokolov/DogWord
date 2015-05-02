c6ba27398beed988f8df6d86eeec2c3142e07c3d
Michael Sokolov <sokolov@falutin.net>
2015-05-02 10:10:26 -0400

    version 1.0.6

---
+++ app/src/main/java/net/falutin/dogword/SettingsActivity.java
@@ -0,0 +1,30 @@
+package net.falutin.dogword;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.preference.PreferenceFragment;
+
+/**
+ * Handle settings / preferences
+ */
+
+public class SettingsActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        // Display the fragment as the main content.
+        getFragmentManager().beginTransaction()
+                .replace(android.R.id.content, new SettingsFragment())
+                .commit();
+    }
+
+    public static class SettingsFragment extends PreferenceFragment {
+        @Override
+        public void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            addPreferencesFromResource(R.xml.preferences);
+        }
+    }
+
+}
